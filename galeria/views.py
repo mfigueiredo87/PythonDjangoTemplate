e889482699cf13c46ca5d5648d5218fd93e90d8f
mfigueiredo87 <figtch@gmail.com>
2024-09-04 17:34:44 +0100

    Migration and SQLite viewer

--- galeria/views.py
+++ galeria/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
 def index(request):
-    return render(request, 'galeria/index.html')
+    # estruturas de dados
+    dados = {
+        1: {"nome": "Nebulosa de Carina",
+            "legenda": "webbtelescope.org / NASA / James Webb"},
+        2: {"nome": "Galaxia NGC 1079",
+            "legenda": "nasa.org / NASA / Hubble"}
+    }
+
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
